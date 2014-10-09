0c9f0383e72abf5bf92bf5da59851e2940a0c1e0
ElisabethStereosuper <elisabeth@Chiot.local>
2014-10-09 18:40:46 +0200

    Un peu de tout!

--- js/script.js
+++ js/script.js
@@ -2,11 +2,54 @@
 // variables //
 ///////////////
 
+function burger(){
+	$(".hamburger").find(".symbole-burger").click(function(){
+		$("#menu-header").slideToggle(200).toggleClass('.menuOpen');
+		return false;
+	});
+}
+
+function scrollMenu(){
+	var myScroll = $(document).scrollTop();
+	if (!$("html").hasClass("lt-ie9")) {
+		if (myScroll>10) {
+			if($("html").hasClass("no-touch")){
+				$("#header").addClass("on");
+			}
+		} else {
+			if($("html").hasClass("no-touch") && $("body").hasClass("home")){
+				$("#header").removeClass("on");
+			} 
+		}
+		if (myScroll>100) {
+			if($("html").hasClass("no-touch")){
+				$("#header").addClass("scrolled");
+			}
+		} else {
+			if($("html").hasClass("no-touch")){
+				$("#header").removeClass("scrolled");
+			}
+		}
+	}
+}
 
 $(document).ready(function(){
+	burger();
 
+	$(document).scroll(function() {
+		scrollMenu();
+	});
+	
 });
 
-$( window ).resize(function() {
+function adjustBurgerMenu(){
+	if($(window).width() > 979 && $("#menu-header").not('menuOpen')){
+		$("#menu-header").slideDown(200);
+	}else{
+		$("#menu-header").slideUp(0);
+	}
+}
 
+$( window ).resize(function() {
+	adjustBurgerMenu();
 });
